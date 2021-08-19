fff5cf09bb831cb1bee4bcece88d73efbbb479bb
hmountainn <ghkdwltks123@naver.com>
2021-08-19 22:33:21 +0900

    model

---
+++ src/main/java/com/petharu/web/model/User.java
@@ -0,0 +1,38 @@
+package com.petharu.web.model;
+
+public class User {
+	private String name;
+	private String phone;
+	private String address;
+	
+	public User(String name, String phone, String address) {
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
