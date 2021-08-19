18288b43702e67ee87123ac3193fb2e80dc326cb
hmountainn <ghkdwltks123@naver.com>
2021-08-19 23:31:15 +0900

    Create (post)

--- src/main/java/com/rest/web/controller/UserController.java
+++ src/main/java/com/rest/web/controller/UserController.java
@@ -9,6 +9,8 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.web.model.User;
@@ -22,11 +24,24 @@ public class UserController {
 	@PostConstruct
 	public void init() {
 		userMap = new HashMap<String , User>();
-		userMap.put("1", new User("황지산","010-1234-1111","경남 양산시"));
-		userMap.put("2", new User("김사랑","010-1234-2222","부산광역시 서구"));
-		userMap.put("3", new User("무야호","010-1234-3333","경남 창원시"));
+		userMap.put("1", new User("1","황지산","010-1234-1111","경남 양산시"));
+		userMap.put("2", new User("2","김사랑","010-1234-2222","부산광역시 서구"));
+		userMap.put("3", new User("3","무야호","010-1234-3333","경남 창원시"));
 	}
 	
+	//Create
+	@PostMapping("/user/{id}")
+	public void PostUser(@PathVariable("id") String id,
+						 @RequestParam("name") String name,
+						 @RequestParam("phone") String phone,
+						 @RequestParam("address") String address) {
+		
+		User user = new User(id, name, phone, address);
+		userMap.put(id, user);
+		
+	}	
+	
+	//Read
 	@GetMapping("/user/{id}")
 	public User getUser(@PathVariable("id") String id) {
 		return userMap.get(id);
@@ -40,5 +55,5 @@ public class UserController {
 		
 		return userList;
 	}
-
+	
 }
