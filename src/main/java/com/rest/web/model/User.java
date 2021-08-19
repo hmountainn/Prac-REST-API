dbeae9e7b543073b56ad6d99ca6b4f5d7eb0b659
hmountainn <ghkdwltks123@naver.com>
2021-08-19 23:23:23 +0900

    user id add

--- src/main/java/com/rest/web/model/User.java
+++ src/main/java/com/rest/web/model/User.java
@@ -1,15 +1,25 @@
 package com.rest.web.model;
 
 public class User {
+	private String id;
 	private String name;
 	private String phone;
 	private String address;
 	
-	public User(String name, String phone, String address) {
+	public User(String id, String name, String phone, String address) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.phone = phone;
 		this.address = address;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -35,4 +45,6 @@ public class User {
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	
+	
 }
